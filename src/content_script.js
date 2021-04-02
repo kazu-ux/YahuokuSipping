7f8c05564399db48cf6cb4844e94c557f230b5f8
kazu-ux <61073127+kazu-ux@users.noreply.github.com>
2021-04-03 08:42:20 +0900

    リファクタリング、税の有無判定

--- src/content_script.js
+++ src/content_script.js
@@ -1,29 +1,47 @@
 
-//送料込み価格の項目を追加する
+//送料込み価格の項目と送料入力ボックスを追加する
 const addShipping = () => {
     document
         .querySelector(".Price__value")
         .insertAdjacentHTML("afterend",
-            '<dt class="Price__title">送料込み価格</dt>' +
-            '<dd class="Price__value" id="SumShipping">----円</dd>' +
-            '<input type="number" name="shipping" value="">円')
+            '<dt class="Price__title">送料・税込み価格</dt>' +
+            '<dd class="Price__value" id="SumShipping">----円</dd>')
+}
+
+//送料を入力するフォームを作る
+const createShippingForm = () => {
+    document.querySelector('#SumShipping').insertAdjacentHTML('afterend', '<input type="number" name="shipping" value="660">円')
+    const shippingForm = document.querySelector("dl > input[type=number]");
+    getInputValue();
+    shippingForm.addEventListener('input', () => {
+        getInputValue();
+    })
+}
+
+//現在価格、または税込み価格を返す関数
+const returnPrice = () => {
+    const tax_price = document.querySelector('.Price__tax').textContent.replace(/[^0-9]/g, '');
+    const str_price = document.querySelector('.Price__value').firstChild.data //○,○○○円
+    const num_price = str_price.replace(/[,円]/g, '')
+    if (tax_price === '0') {
+        return num_price;
+    } else {
+        return tax_price;
+    }
+}
+
+//入力ボックスの値を取得する
+const getInputValue = () => {
+    const SumShippingArea = document.querySelector('#SumShipping');
+    const shippingForm = document.querySelector("dl > input[type=number]");
+    const shippingPlusPrice = Number(shippingForm.value) + Number(returnPrice());
+    SumShippingArea.textContent = String(shippingPlusPrice) + "円";
+}
+
+const main = () => {
+    addShipping();
+    createShippingForm();
 }
 
 //HTMLの読み込みが完了してから
-document.addEventListener('DOMContentLoaded', addShipping());
-
-//送料を入力するフォームを作る
-const shippingForm = document.querySelector("dl > input[type=number]");
-console.log(shippingForm);
-shippingForm.addEventListener('input', () => {
-    const SumShippingArea = document.querySelector('#SumShipping');
-    const shippingPlusPrice = Number(shippingForm.value) + Number(getPrice());
-    SumShippingArea.textContent = String(shippingPlusPrice) + "円";
-})
-
-//現在価格を取得する関数
-const getPrice = () => {
-    const str_price = document.querySelector('.Price__value').firstChild.data //○,○○○円
-    const num_price = str_price.replace(/[,円]/g, '')
-    return (num_price);
-}
+document.addEventListener('DOMContentLoaded', main());
