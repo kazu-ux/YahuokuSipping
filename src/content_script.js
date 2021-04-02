a25c1c3ffd0672df125bede7ced2a5da039d9a57
kazu-ux <61073127+kazu-ux@users.noreply.github.com>
2021-04-02 20:13:33 +0900

    scriptの実行タイミングを変更

--- src/content_script.js
+++ src/content_script.js
@@ -1,17 +1,24 @@
+
 //送料込み価格の項目を追加する
-document
-    .querySelector(".Price__value")
-    .insertAdjacentHTML("afterend",
-        '<dt class="Price__title">送料込み価格</dt>' +
-        '<dd class="Price__value" id="SumShipping">4,300円</dd>' +
-        '<input type="number" name="shipping" value="">円')
+const addShipping = () => {
+    document
+        .querySelector(".Price__value")
+        .insertAdjacentHTML("afterend",
+            '<dt class="Price__title">送料込み価格</dt>' +
+            '<dd class="Price__value" id="SumShipping">----円</dd>' +
+            '<input type="number" name="shipping" value="">円')
+}
+
+//HTMLの読み込みが完了してから
+document.addEventListener('DOMContentLoaded', addShipping());
 
 //送料を入力するフォームを作る
 const shippingForm = document.querySelector("dl > input[type=number]");
 console.log(shippingForm);
 shippingForm.addEventListener('input', () => {
     const SumShippingArea = document.querySelector('#SumShipping');
-    SumShippingArea.textContent = Number(shippingForm.value) + Number(getPrice());
+    const shippingPlusPrice = Number(shippingForm.value) + Number(getPrice());
+    SumShippingArea.textContent = String(shippingPlusPrice) + "円";
 })
 
 //現在価格を取得する関数
