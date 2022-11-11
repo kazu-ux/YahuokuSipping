0ff0059438d0f104d36a202c1a0d32af6d4b14f1
kazu-ux <61073127+kazu-ux@users.noreply.github.com>
2022-11-11 14:32:49 +0900

    clean: リファクタリング

--- src/background.ts
+++ src/background.ts
@@ -1,13 +1,3 @@
-type Details = {
-  name: string;
-  url: string;
-  value?: string;
-  expirationDate?: number;
-};
-
-const setCookies = (details: Details) => {
-  chrome.cookies.set(details);
-};
 //現在時刻をUnixtimeで返す
 const getUnixTime = () => {
   const date = new Date();
@@ -43,5 +33,3 @@ chrome.runtime.onMessage.addListener(
     return true;
   }
 );
-
-export {};
